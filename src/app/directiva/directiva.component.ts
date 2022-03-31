4236fdfa61d6ffb7bf69c283b2ebce18e75d612e
gersondlarosa <gerson.dlarosa@gmail.com>
2022-03-30 18:13:18 -0600

    Se agrega proyecto Angular

---
+++ src/app/directiva/directiva.component.ts
@@ -0,0 +1,21 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'app-directiva',
+  templateUrl: './directiva.component.html'
+})
+export class DirectivaComponent  {
+
+  listaCurso: string[] = ['TypeScript','JacaScript','Java SE', 'C#', 'PHP'];
+
+  habilitar: boolean = true;
+
+  constructor() { }
+
+  setHabilitar(): void{
+  this.habilitar = (this.habilitar == true)? false: true; 
+  }
+
+
+
+}
